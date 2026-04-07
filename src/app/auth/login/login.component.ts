ab72ea66ff7d49bb3243861938c0283a48948b79
Flaviobrdf <flaviobrdf@hotmail.com>
2026-04-06 23:25:19 -0300

    Página de Login

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  onSubmit(event: Event) {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const email = (form.elements.namedItem('email') as HTMLInputElement).value;
+    const password = (form.elements.namedItem('password') as HTMLInputElement).value;
+
+    console.log('Tentando login com:', email, password);
+  }
+
+  onRegister() {
+    console.log('Ir para tela de registro');
+  }
+}
